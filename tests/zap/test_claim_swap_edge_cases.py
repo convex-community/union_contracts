ff5d7519dd8db7c1054cf23e2674e13bf1a2697f
benber86 <benoitb@tutanota.de>
2022-01-17 22:54:45 +1100

    Add test for slippage protection with locking

--- tests/zap/test_claim_swap_edge_cases.py
+++ tests/zap/test_claim_swap_edge_cases.py
@@ -51,6 +51,52 @@ def test_claim_and_swap_no_discount(
     chain.revert()
 
 
+def test_claim_and_swap_no_discount_slippage_protection(
+    owner, union_contract, set_mock_claims, claim_tree, merkle_distributor_v2, vault
+):
+    chain.snapshot()
+    network.gas_price("0 gwei")
+    original_union_balance = interface.IERC20(CVXCRV).balanceOf(union_contract)
+    merkle_distributor_v2.unfreeze({"from": owner})
+
+    proofs = claim_tree.get_proof(union_contract.address)
+    params = [
+        [token, proofs["claim"]["index"], CLAIM_AMOUNT, proofs["proofs"]]
+        for token in TOKENS
+    ]
+    crv = interface.IERC20(CRV)
+    crv.approve(CURVE_CVXCRV_CRV_POOL, 2 ** 256 - 1, {"from": CURVE_VOTING_ESCROW})
+    cvxcrv_swap = interface.ICurveFactoryPool(CURVE_CVXCRV_CRV_POOL)
+    cvxcrv_swap.add_liquidity(
+        [crv.balanceOf(CURVE_VOTING_ESCROW), 0],
+        0,
+        CURVE_VOTING_ESCROW,
+        {"from": CURVE_VOTING_ESCROW},
+    )
+
+    expected_output_amount, eth_crv_ratio = estimate_output_amount(
+        TOKENS, union_contract, 0
+    )
+    union_contract.setApprovals({"from": owner})
+    with brownie.reverts():
+        tx = union_contract.distribute(
+            params, 0, True, True, True, expected_output_amount * 1.25, {"from": owner}
+        )
+    tx = union_contract.distribute(
+        params, 0, True, True, True, expected_output_amount * 0.75, {"from": owner}
+    )
+    distributor_balance = vault.balanceOfUnderlying(merkle_distributor_v2)
+    union_balance = (
+        interface.IERC20(CVXCRV).balanceOf(union_contract) - original_union_balance
+    )
+    assert distributor_balance > 0
+    assert union_balance > 0
+    assert merkle_distributor_v2.frozen() == True
+    assert union_balance + distributor_balance == expected_output_amount
+    assert tx.events[-1]["locked"] == True
+    chain.revert()
+
+
 def test_claim_and_swap_not_owner(
     alice, owner, set_mock_claims, claim_tree, union_contract
 ):
