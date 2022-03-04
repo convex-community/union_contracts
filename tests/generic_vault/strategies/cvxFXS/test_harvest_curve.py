e95143d896e8d9ee5f96a05a6fcce56f5e129a72
benber86 <benoitb@tutanota.de>
2022-03-04 21:59:45 +1100

    Test harvest (Curve harvest)

--- tests/generic_vault/strategies/cvxFXS/test_harvest_curve.py
+++ tests/generic_vault/strategies/cvxFXS/test_harvest_curve.py
@@ -7,6 +7,7 @@ from ....utils.cvxfxs import calc_harvest_amount_curve, estimate_lp_tokens_recei
 
 def test_harvest_single_staker(alice, bob, owner, vault, strategy):
     chain.snapshot()
+    strategy.setSwapOption(0, {"from": owner})
     alice_initial_balance = cvxfxs_lp_balance(alice)
     bob_initial_balance = fxs_balance(bob)
     platform_initial_balance = fxs_balance(vault.platform())
@@ -40,4 +41,54 @@ def test_harvest_single_staker(alice, bob, owner, vault, strategy):
         alice_initial_balance + estimated_harvest,
         1e-5,
     )
+    chain.revert()
+
+
+def test_harvest_multiple_stakers(
+    alice, bob, charlie, dave, erin, owner, vault, strategy
+):
+    chain.snapshot()
+    strategy.setSwapOption(0, {"from": owner})
+    initial_balances = {}
+    accounts = [alice, bob, charlie, dave, erin]
+
+    for account in accounts:
+        initial_balances[account.address] = cvxfxs_lp_balance(account)
+        vault.depositAll(account, {"from": account})
+
+    bob_initial_balance = fxs_balance(bob)
+    platform_initial_balance = fxs_balance(vault.platform())
+    initial_vault_balance = vault.totalUnderlying()
+
+    chain.sleep(100000)
+    chain.mine(1)
+    estimated_harvested_fxs = calc_harvest_amount_curve(strategy, True)
+
+    platform_fees = estimated_harvested_fxs * vault.platformFee() // 10000
+    caller_incentive = estimated_harvested_fxs * vault.callIncentive() // 10000
+
+    estimated_harvest = estimate_lp_tokens_received(
+        estimated_harvested_fxs - platform_fees - caller_incentive
+    )
+
+    tx = vault.harvest({"from": bob})
+
+    actual_harvest = tx.events["Harvest"]["_value"]
+    assert approx(estimated_harvest, actual_harvest, 1e-3)
+
+    assert approx(fxs_balance(bob), bob_initial_balance + caller_incentive, 1e-5)
+    assert approx(
+        fxs_balance(vault.platform()), platform_initial_balance + platform_fees, 1e-5
+    )
+    assert approx(
+        vault.totalUnderlying(),
+        initial_vault_balance + estimated_harvest,
+        1e-5,
+    )
+    for account in accounts:
+        assert approx(
+            vault.balanceOfUnderlying(account) - initial_balances[account.address],
+            (estimated_harvest) // len(accounts),
+            1e5,
+        )
     chain.revert()
--- tests/utils/cvxfxs.py
+++ tests/utils/cvxfxs.py
@@ -37,13 +37,14 @@ def calc_harvest_amount_curve(strategy, fxs=False):
     crv_eth_swap = interface.ICurveV2Pool(CURVE_CRV_ETH_POOL)
 
     eth_balance = cvx_eth_swap.get_dy(1, 0, cvx_rewards) if cvx_rewards > 0 else 0
-    eth_balance += crv_eth_swap.get_dy(1, 0, crv_rewards) if crv_rewards > 0 else 0
+    if crv_rewards > 0:
+        eth_balance += crv_eth_swap.get_dy(1, 0, crv_rewards)
 
-    fxs_balance = fxs_rewards + (
-        interface.ICurveV2Pool(CURVE_FXS_ETH_POOL).get_dy(0, 1, eth_balance)
-        if eth_balance > 0
-        else 0
-    )
+    fxs_balance = fxs_rewards
+    if eth_balance > 0:
+        fxs_balance += interface.ICurveV2Pool(CURVE_FXS_ETH_POOL).get_dy(
+            0, 1, eth_balance
+        )
     if fxs:
         return fxs_balance
 
