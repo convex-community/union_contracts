ccdd03ad2a10316950d2b55a4c4bd8766ea60499
benber86 <benoitb@tutanota.de>
2022-03-08 23:28:09 +1100

    Fix error with swap option test due to fn_isolation and for loop

--- tests/generic_vault/strategies/cvxFXS/test_swap_options.py
+++ tests/generic_vault/strategies/cvxFXS/test_swap_options.py
@@ -9,7 +9,8 @@ def test_set_swap_option_non_owner(alice, vault, strategy):
         strategy.setSwapOption(0, {"from": alice})
 
 
-def test_set_swap_option(fn_isolation, owner, vault, strategy):
+def test_set_swap_option(owner, vault, strategy):
+    chain.snapshot()
     for i in range(3):
         tx = strategy.setSwapOption(i, {"from": owner})
         assert strategy.swapOption() == i
@@ -17,3 +18,4 @@ def test_set_swap_option(fn_isolation, owner, vault, strategy):
 
     with brownie.reverts():
         strategy.setSwapOption(4, {"from": owner})
+    chain.revert()
