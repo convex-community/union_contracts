46d337752db15f8fe177f875341b1d8367e5ca65
benber86 <benoitb@tutanota.de>
2022-03-08 22:54:18 +1100

    Update harvest arguments in tests

--- tests/generic_vault/strategies/cvxFXS/test_strategy_access.py
+++ tests/generic_vault/strategies/cvxFXS/test_strategy_access.py
@@ -1,14 +1,14 @@
 import brownie
 from brownie import chain
 
-from ....utils.constants import AIRFORCE_SAFE, ADDRESS_ZERO
+from ....utils.constants import AIRFORCE_SAFE
 
 
 def test_harvest_non_vault(alice, owner, vault, strategy):
     with brownie.reverts("Vault calls only"):
-        strategy.harvest(20, 20, ADDRESS_ZERO, AIRFORCE_SAFE, {"from": owner})
+        strategy.harvest(AIRFORCE_SAFE, {"from": owner})
     with brownie.reverts("Vault calls only"):
-        strategy.harvest(20, 20, ADDRESS_ZERO, AIRFORCE_SAFE, {"from": alice})
+        strategy.harvest(AIRFORCE_SAFE, {"from": alice})
 
 
 def test_withdraw_non_vault(alice, owner, vault, strategy):
