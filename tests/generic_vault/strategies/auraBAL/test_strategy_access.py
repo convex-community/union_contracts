081a1cdb9607b0d815671aefc6bcb9f6c9ccdbb6
benber86 <benoitb@tutanota.de>
2022-07-25 17:17:52 +1000

    Tests permissioned harvest calls

---
+++ tests/generic_vault/strategies/auraBAL/test_strategy_access.py
@@ -0,0 +1,25 @@
+import brownie
+from brownie import chain
+
+from ....utils.constants import AIRFORCE_SAFE
+
+
+def test_harvest_non_vault(alice, owner, vault, strategy):
+    with brownie.reverts("Vault calls only"):
+        strategy.harvest(AIRFORCE_SAFE, {"from": owner})
+    with brownie.reverts("Vault calls only"):
+        strategy.harvest(AIRFORCE_SAFE, {"from": alice})
+
+
+def test_stake_non_vault(alice, owner, vault, strategy):
+    with brownie.reverts("Vault calls only"):
+        strategy.stake(10, {"from": owner})
+    with brownie.reverts("Vault calls only"):
+        strategy.stake(10, {"from": alice})
+
+
+def test_withdraw_non_vault(alice, owner, vault, strategy):
+    with brownie.reverts("Vault calls only"):
+        strategy.withdraw(20, {"from": owner})
+    with brownie.reverts("Vault calls only"):
+        strategy.withdraw(20, {"from": alice})
