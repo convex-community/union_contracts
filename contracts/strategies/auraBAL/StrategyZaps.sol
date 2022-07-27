a422174bd66ca65f2d6dd9f2ae4f5a1934bb4db7
benber86 <benoitb@tutanota.de>
2022-07-27 17:23:42 +1000

    Remove ownership on zaps

--- contracts/strategies/auraBAL/StrategyZaps.sol
+++ contracts/strategies/auraBAL/StrategyZaps.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.9;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "./StrategyBase.sol";
@@ -9,7 +8,7 @@ import "../../../interfaces/IGenericVault.sol";
 import "../../../interfaces/IUniV2Router.sol";
 import "../../../interfaces/IWETH.sol";
 
-contract AuraBalZaps is Ownable, AuraBalStrategyBase {
+contract AuraBalZaps is AuraBalStrategyBase {
     using SafeERC20 for IERC20;
 
     address public immutable vault;
