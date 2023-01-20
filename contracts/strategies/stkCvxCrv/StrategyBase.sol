2b1b85740cadd19f174b3232844e674e9f04b034
benber86 <benoitb@tutanota.de>
2023-01-20 15:25:25 +1100

    Remove unused import

--- contracts/strategies/stkCvxCrv/StrategyBase.sol
+++ contracts/strategies/stkCvxCrv/StrategyBase.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.9;
 
-import "../../../interfaces/IBasicRewards.sol";
 import "../../../interfaces/ICurvePool.sol";
 import "../../../interfaces/ICurveTriCrypto.sol";
 
