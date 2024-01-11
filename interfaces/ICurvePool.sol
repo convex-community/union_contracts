0ff0a030f6c00b44589ea4d9f5679521ce49c5cd
benny <benoitb@tutanota.de>
2024-01-11 23:15:46 +1100

    Basic contracts for stkCvxPrisma vault

--- interfaces/ICurvePool.sol
+++ interfaces/ICurvePool.sol
@@ -20,6 +20,14 @@ interface ICurvePool {
         uint256 min_dy
     ) external returns (uint256);
 
+    function exchange(
+        int128 i,
+        int128 j,
+        uint256 dx,
+        uint256 min_dy,
+        address receiver
+    ) external returns (uint256);
+
     function get_dy(
         int128 i,
         int128 j,
